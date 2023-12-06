bcece429ff2971cd86aade23549fe47fe503fbbb
George Rollason <george.a.rollason@gmail.com>
2023-12-06 23:14:23 +0000

    solve 6pt2

--- 06_waitForIt.js
+++ 06_waitForIt.js
@@ -45,17 +45,44 @@ input.forEach((input) => {
 
 console.log(input.reduce((acc, cur) => acc * cur.solutions, 1));
 
-function solveIntelligently(input) {
+function solveQuadratic(a, b, c) {
+	const discriminant = Math.sqrt(Math.pow(b, 2) - (4*a*c)),
+		divisor = 2*a,
+		lead = -1*b;
+
+	return [(lead - discriminant)/divisor, (lead + discriminant)/divisor];
+}
+
+function solveIntelligently() {
 	// Solve with less brute force for updated input.
 	// Time has 8 characters and distance has 15 characters
 	// Will have to do something smarter.
+	const { time, distance } = newInput;
 
 	// timeHeld * (totalTime - timeHeld) > distance;
 	// solve for timeHeld = x
+	const zeroes = solveQuadratic(1, -time, distance);
 
 	// ax - x^2 - distance > 0;
 	// x^2 - ax + distance < 0;
+	// original quadratic has negative coefficient for power of 2
+	// space between the zeroes is positive.
+
+	return [Math.ceil(zeroes[0]), Math.floor(zeroes[1])];
 }
 
-// how do we solve a quadratic
-// x = -b +- Math.sqrt(b^2 - 4ac) / 2a
+const zeroes = solveIntelligently();
+
+function checkResult(result) {
+	const { time, distance } = newInput;
+	return result * (time - result) - distance;
+}
+console.log(zeroes);
+
+console.log(checkResult(zeroes[0]), checkResult(zeroes[0]+1), checkResult(zeroes[0]-1));
+// zeroes[0] is first real value
+
+console.log(checkResult(zeroes[0]), checkResult(zeroes[0]+1), checkResult(zeroes[0]-1));
+// zeroes[1] is last real value
+
+console.log(zeroes[1] - zeroes[0] + 1); // Have to add 1 because subtraction omits start number of range.
