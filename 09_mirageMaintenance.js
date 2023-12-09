0df370a1df5c002c12b3fa1308b181ab51156b62
George Rollason <george.a.rollason@gmail.com>
2023-12-09 11:48:33 +0000

    day 9: input + pt1 solution. this one was pleasant so far

---
+++ 09_mirageMaintenance.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+
+const input = fs.readFileSync('./inputs/09.txt', {encoding: 'utf-8'});
+const lines = input.split('\r\n').filter(line => line !== '');
+const histories = lines.map(line => line.split(' ')).map(numArray => numArray.map(num => Number(num)));
+
+// Start by analysing successive differences.
+function getDifferenceBetweenTerms(array) {
+  const differences = [];
+  for (let i = 1; i < array.length; i++) {
+    differences.push(array[i] - array[i-1]);
+  }
+  return differences;
+}
+
+function calculateAllDifferences(array) {
+  const obj = {0: array};
+  let i = 0;
+
+  while (!obj[i].every(el => el === 0)) {
+    obj[i + 1] = getDifferenceBetweenTerms(obj[i]);
+    i++;
+  }
+
+  return obj;
+}
+
+const historiesWithDifferences = histories.map(history => calculateAllDifferences(history));
+
+function extrapolateNextValue(historyObject) {
+  // Objects sort numerical keys in ascending order, i.e. we can reliably iterate through Object.keys() in reverse.
+  const keys = Object.keys(historyObject);
+
+  for (let i = keys.length - 1; i >= 0; i--) {
+    const differenceArray = historyObject[keys[i]];
+    if (differenceArray.every(el => el === 0)) {
+      differenceArray.push(0);
+    } else {
+      const lastArray = historyObject[keys[i+1]];
+      const currentArray = historyObject[keys[i]];
+      differenceArray.push(lastArray[lastArray.length - 1] + currentArray[currentArray.length - 1]);
+    }
+  }
+  
+  const history = historyObject[0];
+  return history[history.length - 1];
+}
+
+const extrapolatedValues = historiesWithDifferences.map(historyObject => extrapolateNextValue(historyObject));
+console.log(extrapolatedValues.reduce((acc, value) => acc + value, 0));
